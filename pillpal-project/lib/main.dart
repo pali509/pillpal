64a8cac0b729a7b800641c1f265abceff7466504
pali509 <pablcamp@ucm.es>
2023-10-30 21:11:36 +0100

    Cambio en el main

--- pillpal-project/lib/main.dart
+++ pillpal-project/lib/main.dart
@@ -3,11 +3,14 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'pantalla_inicial.dart';
 import 'pantalla_pastillero.dart';
 import 'navigation_drawer.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(MyApp());
+  Future<void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Supabase.initialize(url: 'https://amwzytiutgstvnrpaiju.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtd3p5dGl1dGdzdHZucnBhaWp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgzNDA3MzQsImV4cCI6MjAxMzkxNjczNH0.-15tfv5ltd59NnnC00FKUL-IsCmWvrpk1y4ktfHA2Y4', );
+      runApp(MyApp());
+  }
 
-}
 
 class MyApp extends StatelessWidget {
   @override
