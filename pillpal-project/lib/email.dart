3f5c08b128bc46b385720b83a6056cf4b0a24a48
MariaCrespo007 <maricr02@ucm.es>
2024-03-05 19:35:22 +0100

    this is me trying

--- pillpal-project/lib/email.dart
+++ pillpal-project/lib/email.dart
@@ -1,26 +1,28 @@
+import 'dart:io';
+
 import 'package:mailer/mailer.dart';
 import 'package:mailer/smtp_server.dart';
 import 'package:mailer/smtp_server/gmail.dart';
 
 import 'package:flutter/material.dart';
 
-String username = 'maricr02@ucm.es';
-String password = '';
-
 void sendPassword(String? email, String? pwd) async{
+  String username = 'pillpal440@gmail.com'; //Your Email
+  String password = 'p1llP@lL013'; // 16 Digits App Password Generated From Google Account
+
   final smtpServer = gmail(username, password);
 
   debugPrint("[ATENCION]: Vamos a por el mail");
 
+  // Create our message.
   final message = Message()
     ..from = Address(username)
-      ..recipients.add('luhere01@ucm.es')
+    ..recipients.add('maricr02@ucm.es')
   /*..ccRecipients.addAll(['destCc1@example.com', 'destCc2@example.com'])
     ..bccRecipients.add(Address('bccAddress@example.com'))*/
     ..subject = 'Recuperación de contraseña'
-    ..text = 'Correo automático de recuperación de contraseña.\nContraseña: $pwd.'
-  //..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>"
-      ;
+    ..text = 'Correo automático de recuperación de contraseña.\nContraseña: $password.';
+
   try {
     final sendReport = await send(message, smtpServer);
     print('Message sent: ' + sendReport.toString());
--- pillpal-project/lib/main.dart
+++ pillpal-project/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pillpal/pantallas/add_calendario_desdePastillero.dart';
 import 'package:pillpal/pantallas/pantalla_informacionMed.dart';
@@ -17,12 +21,29 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:pillpal/database/db_connections.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+//import 'package:pillpal/email.dart';
+
+/*class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+  }
+}*/
+
 Future<void> main() async {
   initDatabaseConnection();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  ByteData data = await PlatformAssetBundle().load('assets/ca/isrg-root-x1-cross-signed.pem');
+  SecurityContext.defaultContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
+
+  //HttpOverrides.global = new MyHttpOverrides();
   //connecting()
   initializeDateFormatting().then((_) => runApp(MyApp()));
   //var moonLanding = DateTime.parse("2020-02-10 12:00:00Z");
 }
+
 final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
